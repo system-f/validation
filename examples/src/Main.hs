79a374d1b6a008428215420bff3a0603ac69aba5
Tony Morris <tmorris@tmorris.net>
2015-02-17 15:28:28 +1000

    some tidying of examples

---
+++ examples/src/Main.hs
@@ -0,0 +1,80 @@
+module Main where
+
+import Control.Lens.Getter((^.))
+import Control.Lens.Iso(from)
+import Control.Lens.Review(( # ))
+import Data.Bifoldable(bitraverse_, bimapM_)
+import Data.Bifunctor(second, first)
+import Data.Validation(AccValidation, Validation, _AccValidation, _Validation, _Success, _Failure)
+
+main ::
+  IO ()
+main =
+  putStrLn "5"
+
+-- Creating Values
+--
+-- Use the _Success and _Failure prisms
+
+successAcc ::
+  AccValidation e String
+successAcc =
+  _Success # "A"
+
+successVal ::
+  Validation e String
+successVal =
+  _Success # "A"
+
+successValT ::
+  Either e String
+successValT =
+  _Success # "A"
+
+failureAcc ::
+  AccValidation Int a
+failureAcc =
+  _Failure # 5
+
+-- | Mapping
+--
+-- The validation types are Functors over the success values,
+-- and are Bifunctors.
+exMapping ::
+  ()
+exMapping =
+  let -- fmap/second are equivalent, and map successes
+      _ = fmap (++ " B" ) successAcc
+      _ = second (++ " B") successAcc
+      _ = first (+1) failureAcc
+      _ = first (+(1 :: Integer)) successAcc -- does nothing
+   in ()
+
+-- | Folding
+--
+-- The Validation types are Bifoldable and Bitraversable
+-- http://hackage.haskell.org/package/bifunctors-4.1.1.1/docs/Data-Bifoldable.html
+-- These typeclasses have rich APIs, and would probably replace most usages of
+-- pattern matching.
+
+exFolding ::
+  IO ()
+exFolding =
+  do
+      bitraverse_ onFailure onSuccess successAcc
+      -- OR
+      bimapM_ onFailure onSuccess successAcc
+        where onFailure _ = putStrLn "Some failure"
+              onSuccess v = putStrLn $ "Good " ++ v
+
+-- | Converting
+-- There are isomorphisms between the validation types, and Either.
+-- 'from' will reverse the isomorphism.
+
+exConvert ::
+  IO ()
+exConvert =
+  do
+      print (successAcc ^. from _AccValidation :: Either Int String)
+      print (successVal ^. from _Validation :: Either Int String)
+      print (Left 3 ^. _Validation :: Validation Int String)
