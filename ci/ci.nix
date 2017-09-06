16946f4570542da165419672efe47673f8e501da
Tony Morris <tmorris@tmorris.net>
2017-09-07 08:08:51 +1000

    ci

---
+++ ci/ci.nix
@@ -0,0 +1,30 @@
+{ supportedSystems ? ["x86_64-linux"]
+, supportedCompilers ? [ "ghc7103" "ghc802" "ghc821" ] 
+}:
+
+with (import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; });
+
+let
+  pkgs = import <nixpkgs> {};
+
+  configurations = 
+    pkgs.lib.listToAttrs (
+      pkgs.lib.concatMap (compiler: 
+        pkgs.lib.concatMap (system: 
+          [{name = "haskell.packages." + compiler + ".validation." + system ; value = {inherit compiler system;};}]
+        ) supportedSystems
+      ) supportedCompilers
+    );
+
+  jobs =
+      pkgs.lib.mapAttrs (name: configuration: 
+          let
+            compiler = configuration.compiler; 
+            system = configuration.system; 
+            nixpkgs = { pkgs = pkgsFor system; };
+            validation = import ../default.nix { inherit nixpkgs compiler; };
+          in
+            validation
+      ) configurations;
+in
+  jobs
---
+++ ci/jobsets.nix
@@ -0,0 +1,27 @@
+{ nixpkgs, declInput }: let pkgs = import nixpkgs {}; in {
+  jobsets = pkgs.runCommand "spec.json" {} ''
+    cat <<EOF
+    ${builtins.toXML declInput}
+    EOF
+    cat > $out <<EOF
+    {
+        "validation": {
+            "enabled": 1,
+            "hidden": false,
+            "description": "validation",
+            "nixexprinput": "validation",
+            "nixexprpath": "ci/ci.nix",
+            "checkinterval": 300,
+            "schedulingshares": 1,
+            "enableemail": false,
+            "emailoverride": "",
+            "keepnr": 5,
+            "inputs": {
+                "validation": { "type": "git", "value": "https://github.com/qfpl/validation", "emailresponsible": false },
+                "nixpkgs": { "type": "git", "value": "https://github.com/NixOS/nixpkgs.git release-17.03", "emailresponsible": false }
+            }
+        }
+    }
+    EOF
+  '';
+}
