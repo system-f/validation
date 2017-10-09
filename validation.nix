203e07d9452a67e6edba58009ea07e172b0ccf27
George Wilson <george@wils.online>
2017-10-10 09:35:55 +1000

    Fix up nix for cabal-doctest

--- validation.nix
+++ validation.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, base, bifunctors, directory, doctest, filepath
+{ mkDerivation, base, bifunctors, cabal-doctest, directory, doctest, filepath
 , lens, mtl, QuickCheck, semigroupoids, semigroups, stdenv
 , template-haskell, transformers
 }:
@@ -10,7 +10,7 @@ mkDerivation {
     base bifunctors lens mtl semigroupoids semigroups transformers
   ];
   testHaskellDepends = [
-    base directory doctest filepath QuickCheck template-haskell
+    base cabal-doctest directory doctest filepath QuickCheck template-haskell
   ];
   homepage = "https://github.com/qfpl/validation";
   description = "A data-type like Either but with an accumulating Applicative";
