1e1e9ca64aa10ffa92710e96478d88fc9d624a19
George Wilson <george@wils.online>
2017-10-10 09:43:00 +1000

    Use cabal2nix

--- validation.nix
+++ validation.nix
@@ -1,16 +1,17 @@
-{ mkDerivation, base, bifunctors, cabal-doctest, directory, doctest, filepath
-, lens, mtl, QuickCheck, semigroupoids, semigroups, stdenv
-, template-haskell, transformers
+{ mkDerivation, base, bifunctors, Cabal, cabal-doctest, directory
+, doctest, filepath, lens, mtl, QuickCheck, semigroupoids
+, semigroups, stdenv, template-haskell, transformers
 }:
 mkDerivation {
   pname = "validation";
   version = "0.5.5";
-  src = ./.;
+  sha256 = "0fgwgpwcisbabzyq11pkj57gp0kydi4px9gmgzqcq2hn6xb43qkd";
+  setupHaskellDepends = [ base Cabal cabal-doctest ];
   libraryHaskellDepends = [
     base bifunctors lens mtl semigroupoids semigroups transformers
   ];
   testHaskellDepends = [
-    base cabal-doctest directory doctest filepath QuickCheck template-haskell
+    base directory doctest filepath QuickCheck template-haskell
   ];
   homepage = "https://github.com/qfpl/validation";
   description = "A data-type like Either but with an accumulating Applicative";
