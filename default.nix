28d4fe0fcb7f6d1c18806b1d979048bd1fca175f
George Wilson <george@wils.online>
2017-10-11 14:28:44 +1000

    Remove unnecessary papa dependency from default.nix. Update SHA in validation.nix

--- default.nix
+++ default.nix
@@ -8,20 +8,7 @@ let
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-  sources = {
-    papa = pkgs.fetchFromGitHub {
-      owner = "qfpl";
-      repo = "papa";
-      rev = "97ef00aa45c70213a4f0ce348a2208e3f482a7e3";
-      sha256 = "0qm0ay49wc0frxs6ipc10xyjj654b0wgk0b1hzm79qdlfp2yq0n5";
-    };
-  };
-
-  modifiedHaskellPackages = haskellPackages.override {
-    overrides = self: super: import sources.papa self // {};
-  };
-
-  validation = modifiedHaskellPackages.callPackage ./validation.nix {};
+  validation = haskellPackages.callPackage ./validation.nix {};
 
 in
 
