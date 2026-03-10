d37578ebb6ffdce19e5bab9b7911d348b0689125
Tony Morris <tmorris@tmorris.net>
2026-03-10 15:48:37 +1000

    delete nix

--- shell.nix
+++
@@ -1,6 +0,0 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
-let
-  inherit (nixpkgs) pkgs;
-  drv = import ./default.nix { inherit nixpkgs compiler; };
-in
-  if pkgs.lib.inNixShell then drv.env else drv
